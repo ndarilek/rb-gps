8ea56c1c771a774e9ce7ec4ab7fd0c20b10656f2
Nolan Darilek <nolan@thewordnerd.info>
2008-11-23 10:44:55 -0600

    Moved Fix attributes into an array.

--- lib/gps/fix.rb
+++ lib/gps/fix.rb
@@ -1,13 +1,32 @@
 # Module representing all data in a GPS fix.
 module Gps::Fix
-	attr_reader :last_tag, :timestamp, :timestamp_error_estimate, :latitude, :longitude, :altitude, :horizontal_error_estimate, :vertical_error_estimate, :course, :speed, :climb, :course_error_estimate, :speed_error_estimate, :climb_error_estimate, :satellites
 
-	def initialize(*args)
-		@altitude = 0
-		@latitude = 0
-		@longitude = 0
-		@speed = 0
-		@course = 0
-		@satellites = 0
-	end
+  FIX_ATTRIBUTES = [
+    :last_tag,
+    :timestamp,
+    :timestamp_error_estimate,
+    :latitude,
+    :longitude,
+    :altitude,
+    :horizontal_error_estimate,
+    :vertical_error_estimate,
+    :course,
+    :speed,
+    :climb,
+    :course_error_estimate,
+    :speed_error_estimate,
+    :climb_error_estimate,
+    :satellites
+  ]
+
+  FIX_ATTRIBUTES.each { |v| attr_reader v }
+
+  def initialize(*args)
+    @altitude = 0
+    @latitude = 0
+    @longitude = 0
+    @speed = 0
+    @course = 0
+    @satellites = 0
+  end
 end
